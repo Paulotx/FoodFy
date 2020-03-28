8d0ca4c1abdbe50a5e3654ca18bbbba20e9852fd
Paulotx <pauloandre0011@gmail.com>
2020-03-28 15:44:03 -0300

    Adicionando videos em destaque no home

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express  = require('express');
 const nunjucks = require('nunjucks');
 
+const receitas = require('./data');
+
 const server = express();
 
 server.use(express.static('public'));
@@ -8,15 +10,17 @@ server.use(express.static('public'));
 server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 });
 
 server.get("/", function(req, res) {
-    return res.render('index');
+    return res.render('index', { items: receitas.reverse() });
 });
 
 server.get("/receitas", function(req, res) {
-    return res.render('receitas');
+    return res.render('receitas', { items: receitas });
 });
 
 server.get("/about", function(req, res) {
