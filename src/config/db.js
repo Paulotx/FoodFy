7cdfdc220691bc9f1ef06057bcabcaed1c2a9a9b
Paulotx <pauloandre0011@gmail.com>
2020-06-15 17:57:33 -0300

    Finalizando DB

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-    user: "postgres",
-    password: "Qwerty@13",
+    user: "",
+    password: "",
     host: "localhost",
     port: 5432,
-    database: "foodfy2"
+    database: ""
 });
